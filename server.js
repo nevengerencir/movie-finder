7d86d15a7ad90ae34d71073746a5dc239ce181cc
NevenGerencir <gerencirn@gmail.com>
2022-10-17 17:00:12 +0200

    A simple autofilling movie searcher app

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 8000
+const{ MongoClient, ObjectId} = require('mongodb')
+const { urlencoded, response } = require('express')
+require('dotenv').config()
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'sample_mflix',
+    collection
+MongoClient.connect(dbConnectionStr)
+.then(client =>{
+    console.log(`connected to database`)
+    db = client.db(dbName)
+    collection = db.collection('movies')
+})
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
+app.get('/search',async (request,response) =>{
+    try{
+        const result = await collection.aggregate([
+            {
+                "$Search":{
+                    "autocomplete":{
+                        "query": `${request.query.query}`,
+                        "path": "title",
+                        "fuzzy":{
+                            "MaxEdits" : 2,
+                            "prefixLength" : 3
+                        }
+                    }
+                }
+        }
+    ]).toArray()
+    response.send(result)
+    }
+    catch (error) {
+        response.status(500).send({message: error.message})
+    }
+
+    })
+    app.get('/get/:id', async (request,response) => {
+        try{ 
+           let result = await collection.findOne({ "_id": ObjectId(request.params.id)})
+           response.send(result)
+
+        }
+        catch(error) {
+            response.status(500).send({message: error.message})
+        }
+    })
+app.listen(process.env.PORT || PORT, () =>{
+    console.log(`Server is running`)
+})
+
+ 
